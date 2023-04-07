f202726fb022f2c3a61fc6fc947c241ebcd85771
dzulfaqaar <fikri6aul@gmail.com>
2023-04-07 09:59:59 +0700

    Add project files.

---
+++ easd_1_180_D/easd_1_180_D.cpp
@@ -0,0 +1,6 @@
+// 1. algoritma dapat memungkinkan progammer untuk memecahkan masalah menjadi langkah langkah yang lebih kecil dan mudah dikelola, sehingga mereka dapat merancang program yang lebih terstruktur dan mudah dipelihara
+// 2. linier dan non-linier atau statis dan dinamis
+// 3. ukuran input, algoritma yang dipakai, struktur data, bahasa pemrograman,optimisasi kode
+// 4. insertion sort karena  ketika mengurutkan data yang ukurannya kecil, insertion sort membutuhkan jumlah iterasi yang relatif sedikit dibandingkan dengan algoritma pengururutan lainnya
+// 5. quadratic (bubble sort, selection sort, insertion sort), loglinear ( merge sort, quick sort)
+
